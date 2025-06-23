12ea11cd4eed0934a299f7c2675c0c6b85c4bcaa
NguyenVietHung <85593777+hungambrose123@users.noreply.github.com>
2025-06-23 16:35:39 +0700

    Revert "feat: add Admin login page v1.0, add private and public routes and in…"

--- app/src/routes/PrivateRoutes.tsx
+++ app/src/routes/PrivateRoutes.tsx
@@ -1,16 +1,10 @@
-import Layout from '@/components/layouts/layout'
-import AdminDashboard from '@/pages/Admin/AdminDashboard'
 import React from 'react'
-import { Route } from 'react-router-dom'
 
 function PrivateRoutes() {
   return (
-    <>
-      <Route path="/admin" element={<Layout children={undefined} />}>
-          <Route path="dashboard" element={<AdminDashboard />}></Route>
-      </Route>
+    <div>
       
-    </>
+    </div>
   )
 }
 
